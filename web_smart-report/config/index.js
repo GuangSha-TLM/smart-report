1a88ca8d8e91c585e17db964c2ff0ea46a0ae81b
hln <128910620+1920604577@users.noreply.github.com>
2024-11-17 21:44:05 +0800

    收尾 1.0
    收尾 1.0

--- web_smart-report/config/index.js
+++ web_smart-report/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
     proxyTable: {
       // 设置代理请求
       "/api": {
-        target: "http://192.168.0.211:8079", // 接口地址
-        // target: "http://127.0.0.1:8079", // 接口地址
+        // target: "http://192.168.0.211:8079", // 接口地址
+        target: "http://127.0.0.1:8079", // 接口地址
         changeOrigin: true, //是否跨域
         pathRewrite: {
           "^/api": "", //本身的接口地址没有"/api"这种通用前缀,所以要rewrite,如果本身有则去掉.
